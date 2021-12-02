2531525e41c987d70f05e1c96172d2f97244885c
Niko D <nikodojan@live.de>
2021-12-02 10:03:47 +0100

    component example

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <mymap :parking="dataList"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import mymap from './components/Map.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, mymap
+  },
+  data (){ 
+    return {
+    dataList: [1,2,3]
+  }
+    
   }
 }
 </script>
---
+++ src/components/Map.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="p, index in parking" :key="index">{{p}}</div>
+</template>
+
+<script>
+export default {
+    name: 'mymap',
+    data () {
+        return{
+            
+        }
+    },
+    props: {
+        parking: []
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
