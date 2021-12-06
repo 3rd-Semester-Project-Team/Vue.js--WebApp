da602653539ed66944af88cbc5f747110ebf53f3
sarahBæk2021 <sbaek18@gmail.com>
2021-12-06 11:28:18 +0100

    waether api

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -3,9 +3,11 @@
   <div class="d-flex flex-row w-100 justify-content-right">
       <div class="col text-right">
           <p>{{date()}}</p>
+           <p v-if="weather != null">{{weather.main.temp}} &#176;C {{weather.weather[0].main}}  </p>
+
       </div>
       <div class="col text-right">
-          image
+          <img v-if="weather != null" v-bind:src="'https://openweathermap.org/img/w/' + weather.weather[0].icon + '.png'">
           </div>
   </div>
 </div>
@@ -15,6 +17,11 @@
 export default {
     name: 'weather',
     data () {
+        return {
+            weather : null 
+
+            
+        }
 
     },
     methods: {
@@ -23,9 +30,21 @@ export default {
             return date.toLocaleDateString()
         },
         getWeather: function() {
-            axios.get()
+            axios.get("https://api.openweathermap.org/data/2.5/weather?q=Roskilde&appid=10edbc177a0e566ad00fac132ccb8e0d&units=metric")
+           .then(response=>{
+        console.log(response.status)
+        this.weather = response.data
+        console.log(this.weather)
+      })
+      .catch(function(error){
+        console.log(error)
+      })
         }
-    }
+    },
+     created () {
+    // is called when this component is created
+    this.getWeather()
+  }
 }
 </script>
 
